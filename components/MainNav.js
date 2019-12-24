eb64800757a6ba25868312445aab790da87ccf56
Klitb <sapondanai.kb@mail.kmutt.ac.th>
2019-12-24 12:35:08 +0700

    [ADD] Component file, MainNav

---
+++ components/MainNav.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Button } from 'reactstrap'
+
+const Example = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">reactstrap</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/">HOME</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/">WHAT</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href ="/">WHO</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href ="/">BRANCH</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href ="/">WHERE</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href ="/">WHEN</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href ="/">FAQS</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href ="/">GAME</NavLink>
+            </NavItem>
+          </Nav>
+          <NavbarText>Simple Text</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Example;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
+import MainNav from '../components/MainNav'
 
 export default class index extends Component {
     render() {
         return (
             <div>
+                <MainNav />
                 <h1>WIP Camp</h1>
             </div>
         )
