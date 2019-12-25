1513a1cd111a64e017fa5a36ef1206fb4949cb2b
Pathinya <gift_01210@hotmail.com>
2019-12-25 16:15:11 +0700

    [UPDATE] add title of the web

--- pages/_document.js
+++ pages/_document.js
@@ -6,9 +6,10 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
-        <meta charset="utf-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-    <link rel="stylesheet" href="\node_modules\bootstrap\dist\css\bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"/>
+          <meta charset="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+          <link rel="stylesheet" href="\node_modules\bootstrap\dist\css\bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous" />
+          <title>WIP Camp #12 : Ways to IT Professionals Camp</title>
         </Head>
         <body>
           <Main />
