1a132778cb5a07f3058d2ec62488d748db0c1600
Pathinya <gift_01210@hotmail.com>
2019-12-25 17:06:49 +0700

    [UPDATE] changed from using flex class to media class

--- components/ImgInfo.js
+++ components/ImgInfo.js
@@ -3,21 +3,23 @@ import styled from 'styled-components'
 import { Content } from './Text'
 
 const Image = styled.img`
-margin-right: 50px;
-margin-top: 50px;
+margin-right: 5%;
 width: 30%;
-
-
 `
 export default class ImgInfo extends Component {
     render() {
         return (
-
-            <div class="d-flex" style={{ margin: "0 0 20% 0" }}>
+            
+            <div className="media">
                 <Image src={this.props.src} />
-                <Content content={this.props.info || "Unknow"} color={this.props.color}/>
+                <div className="media-body">
+                    <Content 
+                        content={this.props.info || "Unknow"}
+                        color={this.props.color}
+                        />  
+                </div>
             </div>
-
+           
         )
     }
 }
