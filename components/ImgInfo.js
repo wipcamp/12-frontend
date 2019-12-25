cf7918b83dd48907d9dcc3ec0e7be2912eac1f25
Pathinya <gift_01210@hotmail.com>
2019-12-25 16:34:58 +0700

    [UPDATE] In ImgInfo.js, change from using Text with styled-component to Content from Text.js

--- components/ImgInfo.js
+++ components/ImgInfo.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import { Content } from './Text'
 
-const Text = styled.p`
-font-size:auto;
-
-`
 const Image = styled.img`
 margin-right: 50px;
 margin-top: 50px;
@@ -15,12 +12,12 @@ width: 30%;
 export default class ImgInfo extends Component {
     render() {
         return (
-        
-               <div class="d-flex" style={{margin:"0 0 20% 0"}}>
-               <Image src={this.props.src} />
-    <Text>{this.props.info || "Unknow"}</Text>               
-  </div> 
-        
+
+            <div class="d-flex" style={{ margin: "0 0 20% 0" }}>
+                <Image src={this.props.src} />
+                <Content content={this.props.info || "Unknow"} color={this.props.color}/>
+            </div>
+
         )
     }
 }
