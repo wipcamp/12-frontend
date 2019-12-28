c9a1479ae3725a1f80ce5e4ad775ea4f44ed7ae8
Pathinya <gift_01210@hotmail.com>
2019-12-28 18:36:05 +0700

    [UPDATE] Make the card flip when hover

--- components/Card.js
+++ components/Card.js
@@ -1,6 +1,43 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const CardContainer = styled.div`
+    position: relative;
+    z-index: 1;
+    width: ${props => props.w};
+    perspective: 1000px;
+    
+`
+
+const CardBody = styled.div`
+    width: 290px;
+    height: 100%;
+    transform-style: preserve-3d;
+    transition: all .7s linear;
+    :hover {
+        transform: rotateY(180deg);
+    }
+`
+
+const CardFront = styled.div`
+    :hover {
+        opacity: 0;
+        visibility: hidden;
+        transition: opacity .25s ease-in, visibility .75s linear;
+    }
+`
+
+const CardBack = styled.div`
+    z-index: 2;
+    transform: rotateY(180deg);
+`
+
+const CardSide = styled.div`
+    position: absolute;
+    top: 0;
+    backface-visibility: hidden;
+`
+
 const TrackCardStyled = styled.img`
     width: 290px;
     height: 521px;
@@ -12,17 +49,43 @@ const FaqsCardStyled = styled.img`
 `
 
 export const TrackCard = (props) => {
-    
-        return (
-            <TrackCardStyled src={props.src}/>
-        )
-    
+
+    return (
+        <CardContainer w="290px">
+            <CardBody>
+                <CardSide>
+                    <CardBack>
+                        <TrackCardStyled src={props.srcBack} />
+                    </CardBack>
+                </CardSide>
+                <CardSide>
+                    <CardFront>
+                        <TrackCardStyled src={props.srcFront} />
+                    </CardFront>
+                </CardSide>
+            </CardBody>
+        </CardContainer>
+    )
+
 }
 
 export const FaqsCard = (props) => {
-    
+
     return (
-        <FaqsCardStyled src={props.src}/>
+        <CardContainer w="294px">
+            <CardBody>
+                <CardSide>
+                    <CardBack>
+                        <FaqsCardStyled src={props.srcBack} />
+                    </CardBack>
+                </CardSide>
+                <CardSide>
+                    <CardFront>
+                        <FaqsCardStyled src={props.srcFront} />
+                    </CardFront>
+                </CardSide>
+            </CardBody>
+        </CardContainer>
     )
 
 }
