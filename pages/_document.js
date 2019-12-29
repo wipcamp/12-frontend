c37adb8ef03c1e737da8536564870547549d4d86
Pathinya <gift_01210@hotmail.com>
2019-12-30 00:01:42 +0700

    [UPDATE] fixed the warning when using yarn dev

--- pages/_document.js
+++ pages/_document.js
@@ -7,9 +7,9 @@ export default class MyDocument extends Document {
       <html>
         <title>WIP Camp #12 : Ways to IT Professionals Camp</title>
         <Head>
-          <meta charset="utf-8" />
+          <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-          <link rel="stylesheet" href="\node_modules\bootstrap\dist\css\bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous" />
+          <link rel="stylesheet" href="\node_modules\bootstrap\dist\css\bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossOrigin="anonymous" />
           
         </Head>
         <body>
