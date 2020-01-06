2174cfecdfcf5a5a4bb20b72af41cc84809c18f2
Pathinya <gift_01210@hotmail.com>
2020-01-06 18:10:58 +0700

    [UPDATE] change navbar hover

--- components/MainNav.js
+++ components/MainNav.js
@@ -14,30 +14,43 @@ import { Button } from 'reactstrap'
 import styled from 'styled-components'
 import 'bootstrap/dist/css/bootstrap.min.css'
 const StyledNavItem = styled(NavItem)`
-    padding : 0 10px 0 10px;
+    padding-left: 24.5px;
+    padding-right: 24.5px;
     text-decoration: none;
     height: 100%;
+
+    :hover {
+      background-color: pink;
+    }
     
 `
 
 const StyledNavLink = styled(NavLink)`
+padding : 40px 24.5px 40px 24.5px;
   color : red;
   :hover {
     color: blue;
-    
-    
   }
 `
 
 const StyledNavbarBrand = styled(NavbarBrand)`
+  
 `
 
 const ColorNav = styled(Navbar)`
-  padding: 20px 94px 17px 100px;
-  background-color : transparent; 
+  margin: 0;
+  padding: 0;
+  background-color : yellow; 
 `
 const StyledImg = styled.img`
-    height : px;
+    margin-top: 20px;
+  margin-left: 100px;
+  margin-bottom: 17px;
+  
+`
+
+const StyledButton = styled(Button)`
+  margin: 24px 94px 24px 0;
 `
 
 const Example = (props) => {
@@ -51,7 +64,7 @@ const Example = (props) => {
         <StyledNavbarBrand href="/" className="mr-auto"><StyledImg src="/Logo2.png"/></StyledNavbarBrand>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
-          <Nav className="ml-auto" navbar>
+          <Nav className="m-auto" navbar>
             <StyledNavItem>
               <Link href="/index">
               <StyledNavLink href= "/" >HOME</StyledNavLink>
@@ -81,10 +94,10 @@ const Example = (props) => {
               <StyledNavLink href ="/">GAME</StyledNavLink>
             </StyledNavItem>
             <StyledNavItem>
-              <StyledNavLink href ="/contact">Contact</StyledNavLink>
+              <StyledNavLink href ="/contact">CONTACT</StyledNavLink>
             </StyledNavItem>
           </Nav>
-          <Button className ="ml-auto" color ="warning"><NavbarText>Register</NavbarText></Button>
+          <StyledButton color ="warning" ><NavbarText>Register</NavbarText></StyledButton>
         </Collapse>
 
       </ColorNav>
