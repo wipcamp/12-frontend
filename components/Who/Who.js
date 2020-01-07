a06e2b3a27ea5925cc3f4aa0325f01fe64c107bf
akira2542 <akiraakamura@gmail.com>
2020-01-07 16:01:34 +0700

    [ADD] Who.js Layout

---
+++ components/Who/Who.js
@@ -0,0 +1,33 @@
+import React, { Component, Fragment } from 'react'
+import styled from 'styled-components'
+import {Container,Row,Col} from 'reactstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Header , SubHeader} from '../Core/Text'
+import ImgInfo from '../Core/ImgInfo'
+
+export default class Who extends Component {
+    render () {
+        return(
+            <Fragment>
+                <Container>
+                    <Row>
+                        <Col sm="6">
+                            <ImgInfo src="Who/tempcircle.png" info="น้อง ๆที่มีความสนใจด้านไอที หรือต้องการตามหาเส้นทาง เดินของตัวเอง" />
+                        </Col>
+                        <Col sm="6">
+                            <ImgInfo src="Who/tempcircle.png" info="น้อง ๆที่มีความสนใจด้านไอที หรือต้องการตามหาเส้นทาง เดินของตัวเอง" />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col sm="6">
+                            <ImgInfo src="Who/tempcircle.png" info="น้อง ๆที่มีความสนใจด้านไอที หรือต้องการตามหาเส้นทาง เดินของตัวเอง" />
+                        </Col>
+                        <Col sm="6">
+                            <ImgInfo src="Who/tempcircle.png" info="น้อง ๆที่มีความสนใจด้านไอที หรือต้องการตามหาเส้นทาง เดินของตัวเอง" />
+                        </Col>
+                    </Row>
+                </Container>
+            </Fragment>
+        )
+    }
+}
