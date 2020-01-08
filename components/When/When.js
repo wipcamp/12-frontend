8d1f67c75818240a9570d0e88bdabc10ed8a7144
akira2542 <akiraakamura@gmail.com>
2020-01-08 15:46:36 +0700

    [ADD] When.js

---
+++ components/When/When.js
@@ -0,0 +1,39 @@
+import React, { Component, Fragment } from 'react'
+import styled from 'styled-components'
+import {Container,Row,Col} from 'reactstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Header , SubHeader} from '../Core/Text'
+import ImgInfo from '../Core/ImgInfo'
+
+export default class When extends Component {
+    render () {
+        return(
+            <Fragment>
+                <Container>
+                    <Row>
+                        <Col>
+                        <Header stroke="#1E1E1E" headerText="WHEN" />
+                        <SubHeader color="#1E1E1E" subHeaderText="ค่ายจัดเมื่อไหร่" />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col sm="6">
+                            <ImgInfo src="Who/tempcircle.png" info="น้อง ๆที่มีความสนใจด้านไอที หรือต้องการตามหาเส้นทาง เดินของตัวเอง" />
+                        </Col>
+                        <Col sm="6">
+                            <ImgInfo src="Who/tempcircle.png" info="น้อง ๆที่มีความสนใจด้านไอที หรือต้องการตามหาเส้นทาง เดินของตัวเอง" />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col sm="6">
+                            <ImgInfo src="Who/tempcircle.png" info="น้อง ๆที่มีความสนใจด้านไอที หรือต้องการตามหาเส้นทาง เดินของตัวเอง" />
+                        </Col>
+                        <Col sm="6">
+                            <ImgInfo src="Who/tempcircle.png" info="น้อง ๆที่มีความสนใจด้านไอที หรือต้องการตามหาเส้นทาง เดินของตัวเอง" />
+                        </Col>
+                    </Row>
+                </Container>
+            </Fragment>
+        )
+    }
+}
