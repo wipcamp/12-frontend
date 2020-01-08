d9538ef2867eff35d59c37b3c785f894ba4a4ed8
akira2542 <akiraakamura@gmail.com>
2020-01-08 15:50:07 +0700

    [Update] Combine Components

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import SideScroll from '../components/Faqs/SideScroll'
 import GlobalStyles from '../components/Core/GlobalStyle'
-
-
+import When from '../components/When/When'
+import Who from '../components/Who/Who'
+import Game from '../components/Game/Game'
+import Home from '../components/Home/Home'
+import ZodiacRing from '../components/Home/ZodiacRing'
+import Where from './Where'
+import Faqs from '../components/Faqs/Faqs'
+import Sponsor from '../components/Sponsor/Sponsor'
+import MainNav from '../components/Core/MainNav'
 
 
 export default class index extends Component {
 
     render() {
         return (
-            <div>
+            <Fragment>
                 <style jsx global>
                     {GlobalStyles}
                 </style>
-                {/* <MainNav />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <ZodiacRing /> */}
-
-                <SideScroll />
+                <MainNav />
+                <Home/>
+                <Who/>
+                <Where/>
+                <When/>
+                <Faqs/>
+                <Game />
+                <Sponsor />
                 
-            </div>
+            </Fragment>
         )
     }
 }
