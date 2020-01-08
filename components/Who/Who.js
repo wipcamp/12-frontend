78927fd1c580cf3f2e78d97aea35536c3c08a3a8
akira2542 <akiraakamura@gmail.com>
2020-01-08 15:46:24 +0700

    [UPDATE] Who.js

--- components/Who/Who.js
+++ components/Who/Who.js
@@ -1,15 +1,21 @@
 import React, { Component, Fragment } from 'react'
 import styled from 'styled-components'
-import {Container,Row,Col} from 'reactstrap'
+import { Container, Row, Col } from 'reactstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {Header , SubHeader} from '../Core/Text'
+import { Header, SubHeader } from '../Core/Text'
 import ImgInfo from '../Core/ImgInfo'
 
 export default class Who extends Component {
-    render () {
-        return(
+    render() {
+        return (
             <Fragment>
                 <Container>
+                    <Row>
+                        <Col>
+                            <Header stroke="#1E1E1E" headerText="WHO" />
+                            <SubHeader color="#1E1E1E" subHeaderText="ค่ายนี้เหมาะกับใคร" />
+                        </Col>
+                    </Row>
                     <Row>
                         <Col sm="6">
                             <ImgInfo src="Who/tempcircle.png" info="น้อง ๆที่มีความสนใจด้านไอที หรือต้องการตามหาเส้นทาง เดินของตัวเอง" />
