a748cc0fe813280ce487fd4db14970b91d59b5fe
akira2542 <akiraakamura@gmail.com>
2020-01-12 13:47:14 +0700

    [UPDATE] fixed mobile Mainav, almost fully done. started making MainNav for desktop

--- components/Core/MainNav.js
+++ components/Core/MainNav.js
@@ -16,16 +16,12 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 const StyledNavItem = styled(NavItem)`
     text-align:center;
 
+
 	@media (min-width:1024px) {
-		padding-left: 24.5px;
-    	padding-right: 24.5px;
     	text-decoration: none;
     	height: 100%;
 		
 
-    	:hover {
-      	background-color: pink;
-    	}
 	}
     
     
@@ -33,19 +29,39 @@ const StyledNavItem = styled(NavItem)`
 
 const StyledNavLink = styled(NavLink)`
 	font-size: 14px;
-  	color : red !important;
+  	color : black;
   	@media (min-width: 1024px) {
-	 	padding : 34px 24.5px 31px 24.5px;
+	 	/* padding : 34px 24.5px 31px 24.5px; */
+		width:110px !important;
+		height: 86px !important;
 		font-size: 16px;
+		line-height: 70px;
+		border: 1px solid blue;
+		
+
   	}
-  
+
+
+	  
+`
+const Line = styled.div`
+	width:110px;
+	height:10px;
+	background-color: white;
+	position:absolute;
+	bottom:0;
+
+	${StyledNavLink}:hover & {
+		background-color: black;
+	}
 `
 
-const StyledNavLink2 = styled(StyledNavLink)`
+const StyledNavLink2 = styled(NavLink)`
 	font-size: 10px;
-	color: black !important;
+	color: white;
 	padding: 2px 5px;
 	@media (min-width: 1024px) {
+		font-size: 16px;
 		:hover{
 	 		color: white !important;
 		}
@@ -54,33 +70,47 @@ const StyledNavLink2 = styled(StyledNavLink)`
 
 const StyledNavbarBrand = styled(NavbarBrand)`
 	margin: 0;
-	margin-left: 85.96px;
+	padding: 0;
+	/* margin-left: 85.96px; */
 	
   @media (min-width: 1024px) {
-	  
-	  margin: 20px 0 17px 0;
+	  /* margin: 20px 0 17px 0; */
   }
 `
 
 
 const StyledNavbarToggler = styled(NavbarToggler)`
  	border: none;
- 	margin: 15px 0px 15px 17px;
+ 	/* margin: 15px 0px 15px 17px; */
  	padding: 0;
- 	background-image: url("data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba(255,102,203, 1)' stroke-width='3' stroke-linecap='round' stroke-miterlimit='10' d='M4 8h24M4 16h24M4 24h24'/%3E%3C/svg%3E") !important;
-	
+	margin-left: 15px;
+	margin-right: 20px; 
+ 	background-image: url("data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba(255,255,255, 1)' stroke-width='3' stroke-linecap='round' stroke-miterlimit='10' d='M4 8h24M4 16h24M4 24h24'/%3E%3C/svg%3E") !important;
+	background-repeat: no-repeat !important;
+	@media (min-width: 1024px) {
+	  display: none
+  }
 `
 
 const StyledNavbarTogglerOpen = styled(StyledNavbarToggler)`
-	background-image: url("data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba(0,0,0, 1)' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M8 8 L24 24 M8 24 L24 8'/%3E%3C/svg%3E") !important;
-  display: none;
+	background-image: url("data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba(255,255,255, 1)' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M8 8 L24 24 M8 24 L24 8'/%3E%3C/svg%3E") !important;
+  	display: none;
+  	padding: 0;
+	 margin-left: 15px; 
+  	 background-repeat: no-repeat !important;
+	   @media (min-width: 1024px) {
+	  display: none
+  }  
 	
 `
 
 const ColorNav = styled(Navbar)`
   margin: 0;
   padding: 0;
-  background-color : yellow; 
+  background-color : #c4c4c4;
+  transition : 0.5s ease-in-out;
+  -webkit-transition : 0.5s ease-in-out;
+  
 
   @media (min-width: 1024px) {
 	height: 86px;
@@ -88,14 +118,13 @@ const ColorNav = styled(Navbar)`
   
 `
 const StyledImg = styled.img`
-    
 	width: 63px;
 	height: 32px;
 
 	@media (min-width: 1024px) {
-		margin-top: 20px;
+		/* margin-top: 20px;
   		margin-left: 100px;
-  		margin-bottom: 17px;	
+  		margin-bottom: 17px;	 */
   		width: 96px;
   		height: 49px;
 	}
@@ -104,18 +133,58 @@ const StyledImg = styled.img`
 `
 
 const StyledButton = styled(Button)`
-	padding: 0;
+	padding: 0.2em;
 	border-radius: 5px;
-	border-color: black;
-	margin-left: 54px;
+	border-color: #505050;
+	background-color: #505050;
 	font-size: 10px;
+	margin-right:16px;
+
 	
 	@media (min-width: 1024px) {
-		margin: 24px 94px 24px 0;
+		/* margin: 24px 94px 24px 0; */
 		border-radius: 9px;
+		position: absolute;
+		right: 18px;
 		:hover {
 			background-color: black;
 		}
+	}
+  
+`
+const StyledNav = styled(Nav)`
+
+	@media (min-width: 1024px) {
+		width: 100%;
+		height: 86px;
+		border: 1px solid red;
+		display: flex;
+		justify-content: center ; 
+
+	}
+`
+const PlaceHolder = styled.div`
+	display: none;
+@media (min-width: 1024px) {
+	display: block;
+	width: 15%;
+	border:1px solid red;
+}
+`
+
+const NavMobile = styled.div`
+width: 100%;
+display: flex;
+justify-content: space-between;
+padding: 9px 0 8px 0;
+
+@media (min-width: 1024px) {
+		/* margin: 24px 94px 24px 0; */
+		border: 1px solid red;
+		max-width: 100px;
+		padding: 0px;
+		margin-left:5%;
+
 	}
   
 `
@@ -128,25 +197,30 @@ const Navbar1 = (props) => {
 	const changeIconToX = () => {
 		var hamberger = document.getElementById("hamberger");
 		var close = document.getElementById("close");
+		var bgcolor = document.getElementById("bgcolor");
 		if (isOpen == false) {
 			hamberger.style.display = 'none';
 			close.style.display = 'inline';
+			bgcolor.style.backgroundColor = '#918686';
 		} 
 	}
 
 	const changeIconToHamberg = () => {
 		var hamberger = document.getElementById("hamberger");
 		var close = document.getElementById("close");
+		var bgcolor = document.getElementById("bgcolor");
 		if (isOpen == true) {
 			hamberger.style.display = 'inline';
 			close.style.display = 'none';
+			bgcolor.style.backgroundColor = '#c4c4c4'
 		}
 	}
 
+
 	return (
 		<div className="sticky-top">
-			<ColorNav expand="md" color="yellow" >
-				<div id="navbar-mobile">
+			<ColorNav expand="lg" id="bgcolor">
+				<NavMobile>
 					<StyledNavbarToggler onClick={() => { toggle(); changeIconToX();}} id="hamberger"/>
 					<StyledNavbarTogglerOpen  onClick={() => { toggle(); changeIconToHamberg();}} id="close" />
 					<StyledNavbarBrand href="/">
@@ -155,13 +229,15 @@ const Navbar1 = (props) => {
 					<StyledButton color="tranparent">
 						<StyledNavLink2 href="/" >REGISTER</StyledNavLink2>
 					</StyledButton>
-				</div>
+				</NavMobile>
 				<Collapse isOpen={isOpen} navbar>
-					<Nav navbar>
+					<div style={{width:'100%',display:'flex',justifyContent:'center'}}>
+					<StyledNav navbar>
 						<StyledNavItem>
 							<Link href="/index">
 								<StyledNavLink href="/" >HOME</StyledNavLink>
 							</Link>
+							<Line></Line>
 						</StyledNavItem>
 						<StyledNavItem>
 							<Link href="./App">
@@ -189,8 +265,9 @@ const Navbar1 = (props) => {
 						<StyledNavItem>
 							<StyledNavLink href="/contact">CONTACT</StyledNavLink>
 						</StyledNavItem>
-					</Nav>
-
+					</StyledNav>
+					</div>
+					<PlaceHolder></PlaceHolder>
 				</Collapse>
 
 			</ColorNav>
