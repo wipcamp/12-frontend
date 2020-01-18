7c04e38cb7ac14ea05186c9987d9afa743f28068
Pathinya <gift_01210@hotmail.com>
2020-01-18 16:27:36 +0700

    [UPDATE] Game page layout

--- components/Game/index.js
+++ components/Game/index.js
@@ -1,31 +1,52 @@
 import React, { Component, Fragment } from 'react'
 import styled from 'styled-components'
-import {Container,Row,Col} from 'reactstrap'
+import { Row, Col, Button } from 'reactstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {Header , SubHeader} from '../Core/Text'
+import { Header } from '../Core/Text'
 
 const GameContainer = styled.div`
-background-color : #1E1E1E;
-padding: 100px 0 100px 0;
+background: radial-gradient(ellipse at bottom, #1b2735 0%, #090a0f 100%);
+/* max-height: 80vh; */
+
 `
+
+const Wrap = styled.div`
+    background: url(/img/temp.jpg) center no-repeat;
+    position: relative;
+    display: table;
+    width: 100%;
+    height: 584px;
+    background-size: cover;
+    margin-top: 138px;
+`
+
+const StyledButton = styled(Button)`
+    font-family: Chakra Petch;
+    font-size: 48px;
+    color: black;
+    padding: 28px 44px;
+    background-color: white;
+    border-color: white;
+    border-radius: 50px;
+`
+
 export default class Game extends Component {
     render() {
         return (
             <GameContainer>
-                <Container>
-                    <Row>
-                        <Col>
-                        <Header stroke="#ffffff" headerText="GAME"/>
-                        </Col>
-                    </Row>
-                    <Row>
-                    <Col>
-                    <div style={{display:'flex',justifyContent:'Center'}}>
-                    <img src="img/temp.jpg" style={{width:'100%',height:'400px'}}  />
-                    </div>
+
+                <Header stroke="#ffffff" headerText="GAME" />
+                <Row>
+                    <Col xl="1"></Col>
+                    <Col xl="10">
+                        <Wrap>
+                            <div style={{ display: 'table-cell', textAlign: 'center', verticalAlign: 'middle' }}>
+                                <StyledButton>Go To Game</StyledButton>
+                            </div>
+                        </Wrap>
                     </Col>
-                    </Row>
-                </Container>
+                    <Col xl="1"></Col>
+                </Row>
             </GameContainer>
         )
     }
