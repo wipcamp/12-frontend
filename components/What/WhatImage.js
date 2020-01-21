7f22b7dd2c0a4e174e63c55a5d26cf0a9c7dc759
Pathinya <gift_01210@hotmail.com>
2020-01-21 18:31:57 +0700

    [UPDATE] Responsive What page

--- components/What/WhatImage.js
+++ components/What/WhatImage.js
@@ -3,16 +3,26 @@ import styled from 'styled-components'
 
 
 const Image = styled.img`
-    width: 528.51px;
-    height: 397px;
+    width: 168px;
+    height: 116px;
+    scroll-snap-align: center;
     margin: 0px 10px 0px 10px;
-    visibility: ${props => props.visible};
+    
+    @media (min-width: 768px) {
+        width: 393px;
+        height: 284px;
+    }
+
+    @media (min-width: 1024px) {
+        width: 566px;
+        height: 328px;
+    }
 `
 
 export const WhatImage = (props) => {
 
     return (
-        <Image src={props.src} className="slide" visible={props.visible} />
+        <Image src={props.src} className="slide"/>
     )
 }
 Image.defaultProps = {
--- components/What/WhatSideScroll.js
+++ components/What/WhatSideScroll.js
@@ -2,109 +2,64 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 import { WhatImage } from './WhatImage'
 
-
-
 const Wrap = styled.div`
-  overflow-x: scroll;
-  
-  
+    overflow-x: scroll;
+    scroll-snap-type: x mandatory;
     width: 100%;
     position: relative;
     z-index: 1;
-    
     ::-webkit-scrollbar {
          width: 0px;
     }
 `
 
 const ArrowButton = styled.div`
-    height: 63px;
-  width: 63.82px;
+    height: 62.68px;
+    width: 63.5px;
+    align-items: center;
+    justify-content: center;
+    background: rgba(134, 134, 134, 0.68);
+    border-radius: 50%;
+    cursor: pointer;
+    color: white;
+    display: none;
   
-  align-items: center;
-  justify-content: center;
-  background: rgba(134, 134, 134, 0.68);
-  border-radius: 50%;
-  cursor: pointer;
-  color: white;
-  display: none;
-
-  @media (min-width: 1024px) {
-      display: flex;
-  }
-
-  
+    @media (min-width: 1024px) {
+        display: flex;
+    }
 `
-const prev = {
-    position: 'absolute',
-    top: '181px',
-    left: '25px',
-    zIndex: '3',
-    transform: 'rotate(180deg)',
-    fill: 'white'
-}
 
 const next = {
     position: 'absolute',
     fill: 'white',
-    top: '181px',
-    
+    top: '133px',
     right: '17.95px',
     zIndex: '3',
 }
 
-
-
-
-const LeftArrow = (props) => {
-    return (
-        <ArrowButton onClick={props.goToPrevSlide} style={prev}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="31" height="24.93" viewBox="0 0 24 24"><path d="M21 12l-18 12v-24z" /></svg>
-      </ArrowButton>
-    );
-}
-
-
 const RightArrow = (props) => {
     return (
         <ArrowButton onClick={props.goToNextSlide} style={next}>
-
             <svg xmlns="http://www.w3.org/2000/svg" width="31" height="24.93" viewBox="0 0 24 24"><path d="M21 12l-18 12v-24z" /></svg>
-
         </ArrowButton>
     );
 }
 
-
-
 export default class SideScroll extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            images: [
-                { src: "img/temp.jpg"},
-                { src: "img/temp.jpg" },
-                { src: "img/temp.jpg" },
-                { src: "img/temp.jpg" },
-                { src: "img/temp.jpg" },
-                { src: "img/temp.jpg" },
-                { src: "img/temp.jpg" },
-                { src: "img/temp.jpg" }
-            ],
-            currentIndex: 0,
-            translateValue: 0
-        };
-    }
-
-    goToPrevSlide = () => {
-        if (this.state.currentIndex === 0)
-            return;
-
-        this.setState(prevState => ({
-            currentIndex: prevState.currentIndex - 1,
-            translateValue: prevState.translateValue + (this.slideWidth())
-        }))
-    }
+    state = {
+        images: [
+            { src: "img/temp.jpg" },
+            { src: "img/temp.jpg" },
+            { src: "img/temp.jpg" },
+            { src: "img/temp.jpg" },
+            { src: "img/temp.jpg" },
+            { src: "img/temp.jpg" },
+            { src: "img/temp.jpg" },
+            { src: "img/temp.jpg" }
+        ],
+        currentIndex: 0,
+        translateValue: 0
+    };
 
     goToNextSlide = () => {
         if (this.state.currentIndex === this.state.images.length - 1) {
@@ -120,16 +75,14 @@ export default class SideScroll extends Component {
     }
 
     slideWidth = () => {
-        return (document.querySelector('.slide').clientWidth+20)
+        return (document.querySelector('.slide').clientWidth + 20)
     }
 
     render() {
 
         return (
-            <div style={{ position: 'relative', zIndex: '2'}} className="scroll-container">
-                
+            <div style={{ position: 'relative', zIndex: '2' }} className="scroll-container">
                 <Wrap >
-
                     <div
                         style={{
                             display: 'flex',
@@ -137,20 +90,15 @@ export default class SideScroll extends Component {
                             transition: 'transform ease-out 0.45s',
                             padding: '0px',
                             margin: '0px',
-
-                        }} id="item">
+                        }}>
                         {this.state.images.map((data, key) => (
                             <WhatImage
                                 key={key}
                                 src={data.src}
-                                visible={data.visible}
                             />
                         ))}
                     </div>
-
                 </Wrap>
-
-                <LeftArrow goToPrevSlide={this.goToPrevSlide} />
                 <RightArrow goToNextSlide={this.goToNextSlide} />
             </div>
         )
