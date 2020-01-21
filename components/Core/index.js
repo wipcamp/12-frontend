d0b9d33a83c4bdbcefdeef1cc09dae5d646088f0
Pathinya <gift_01210@hotmail.com>
2020-01-21 21:30:38 +0700

    [UPDATE] add appropriate closed tag to index.js in Core

--- components/Core/index.js
+++ components/Core/index.js
@@ -32,7 +32,7 @@ export default class App extends Component {
                 <Game />
                 <Sponsor />    
                 <Contact />            
-            </Fragment>
+            </Wrap>
         )
     }
 }
