782e245a75adb1c773441a30f3ce9e845ea2e741
Meawmfia <boon20002543@hotmail.com>
2020-01-22 18:45:41 +0700

    [UPDATE] set OG

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,12 @@ export default class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+          <meta name="og:title" content="WIP Camp 12" />
+          <meta name="og:description" content="Nulla officia id adipisicing do ipsum laborum tempor labore proident." />
+          <meta name="og:image" content="/img/Who/lungtu.png" />
+          <meta property="og:site_name" content="WIP Camp #12 : Ways to IT Professionals Camp : ค่ายเส้นทางสู่ฝันนักไอที" />
+
+
           <link rel="stylesheet" href="\node_modules\bootstrap\dist\css\bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossOrigin="anonymous" />
           <script src="https://cdn.jsdelivr.net/npm/p5@0.10.2/lib/p5.min.js"></script>
           <script src="sketch.js"></script>
