43f12f6749d99132ee5d0bddf2f60b09efe088ca
akira2542 <akiraakamura@gmail.com>
2020-01-22 08:43:14 +0700

    [UPDATE] When layout

--- components/When/index.js
+++ components/When/index.js
@@ -10,6 +10,24 @@ const WhenContainer = styled.div`
 background-color : #1E1E1E;
 padding: 100px 0 100px 0;
 `
+const WhenImg = styled.img`
+    width:50px;
+    margin: 10px 0 10px 0;
+`
+const TimerText = styled.h4`
+    font-size: 12px;
+    text-align:center ;
+    color: white;
+    font-family:'ChakraPetch-Regular', sans-serif;
+`
+const SmallContainer = styled.div`
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    color: white;
+    flex-wrap: wrap;
+    font-family: 'Chakra Petch', sans-serif;
+`
 
 
 export default class When extends Component {
@@ -23,10 +41,45 @@ export default class When extends Component {
                         <SubHeader color="#ffffff" subHeaderText="ค่ายจัดเมื่อไหร่" />
                         </Col>
                     </Row>
-                    <Row>
-                        <Col>
-                            <WhenTimer />
+                    <Row className="mt-5">
+                        <Col lg=''></Col>
+                        <Col lg=''>
+                        <div className="d-flex flex-column align-items-center">
+                        <WhenImg src='/img/Who/tempcircle.png' />
+                        <TimerText>เปิดรับสมัคร<br/> 12 กุมภาพันธ์ 2563</TimerText>
+                        </div>
                         </Col>
+                        <Col lg=''>
+                        <div className="d-flex flex-column align-items-center">
+                        <TimerText>เปิดรับสมัคร<br/> 12 กุมภาพันธ์ 2563</TimerText>
+                        <WhenImg src='/img/Who/tempcircle.png' />
+                        </div>
+                        </Col>
+                        <Col lg=''>
+                        <div className="d-flex flex-column align-items-center">
+                        <WhenImg src='/img/Who/tempcircle.png' />
+                        <TimerText>เปิดรับสมัคร<br/> 12 กุมภาพันธ์ 2563 นี้ ที่ หี้ </TimerText>
+                            </div>
+                        </Col>
+                        <Col lg=''>
+                        <div className="d-flex flex-column align-items-center">
+                        <TimerText>เปิดรับสมัคร<br/> 12 กุมภาพันธ์ 2563</TimerText>
+                        <WhenImg src='/img/Who/tempcircle.png' />
+                        </div>
+                        </Col>
+                        <Col lg=''>
+                        <div className="d-flex flex-column align-items-center">
+                        <WhenImg src='/img/Who/tempcircle.png' />
+                        <TimerText>เปิดรับสมัคร<br/> 12 กุมภาพันธ์ 2563</TimerText>
+                        </div>
+                        </Col>
+                        <Col lg=''>
+                        <div className="d-flex flex-column align-items-center">
+                        <TimerText>เปิดรับสมัคร<br/> 12 กุมภาพันธ์ 2563</TimerText>
+                        <WhenImg src='/img/Who/tempcircle.png' />
+                        </div>
+                        </Col>
+                        <Col lg=''></Col>
                     </Row>   
                 </Container>
                 
