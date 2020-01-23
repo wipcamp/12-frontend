3467820b1adcb69fbb01617f623682fe77f2e5b7
Pathinya <gift_01210@hotmail.com>
2020-01-23 22:56:48 +0700

    [UPDATE] Sponsor page responsive

--- components/Sponsor/index.js
+++ components/Sponsor/index.js
@@ -2,31 +2,37 @@ import React, { Component } from 'react'
 import { Header, SubHeader } from '../Core/Text'
 import SponsorImage from './SponsorImage'
 import styled from 'styled-components'
-import { Container } from 'reactstrap'
+import { Container, Row, Col } from 'reactstrap'
 
 const SponsorBg = styled.div`
     background: radial-gradient(ellipse at bottom, #1b2735 0%, #090a0f 100%);
-    padding-top: 53px;
+    padding-top: 3em;
+
+    @media (min-width: 768px) {
+        padding-top: 5em;
+    }
+
+    @media (min-width: 1024px) {
+        padding-top: 8em;
+    }
 `
 
 const WrapLogo = styled.div`
     background-color: #c4c4c4;
     text-align: center;
-    padding: 10px;
-    margin-top: 94px;
+    margin: 2em 2.25em 0;
 
     @media (min-width: 768px) {
-        margin-top: 82px;
-        padding: 40px 20px;
+        margin: 2em 3em 0;
     }
     
     @media (min-width: 1024px) {
-        margin-top: 124px;
-        padding: 103px 68px;
+        margin: 3em 0 0;
+        padding: 2.5em;
     }
 `
 export default class Sponsor extends Component {
-    state= {
+    state = {
         images: [
             { src: "/img/Sponsor/alibaba.png" },
             { src: "/img/Sponsor/thaibev.png" },
@@ -36,28 +42,35 @@ export default class Sponsor extends Component {
             { src: "/img/Sponsor/bangmod.png" },
             { src: "/img/Sponsor/pandt.png" },
             { src: "/img/Sponsor/Lnw.png" },
-            { src: "/img/Sponsor/yip.png"  },
+            { src: "/img/Sponsor/yip.png" },
             { src: "/img/Sponsor/dekd.png" },
-            { src: "/img/Sponsor/Lactasoy.png"  },
-            { src: "/img/Sponsor/CPALL.png"  },
+            { src: "/img/Sponsor/Lactasoy.png" },
+            { src: "/img/Sponsor/CPALL.png" },
             { src: "/img/Sponsor/pantip.png" },
-            { src: "/img/Sponsor/central.png"  }
+            { src: "/img/Sponsor/central.png" }
         ]
     };
     render() {
         return (
             <SponsorBg>
-            <div className="text-center">
-                <Header color="white" headerText="SPONSOR" />
-                <SubHeader color="white" subHeaderText="ผู้สนับสนุน" />
-            </div>
-            <Container>
-            <WrapLogo>
-                {this.state.images.map((data, key) => (
-                    <SponsorImage key={key} src={data.src} width={data.width} height={data.height} />
-                ))}
-            </WrapLogo>
-            </Container>
+                <div className="text-center">
+                    <Header color="white" headerText="SPONSOR" />
+                    <SubHeader color="white" subHeaderText="ผู้สนับสนุน" />
+                </div>
+                
+                    <Row>
+                        <Col xl="1" lg="1" md="1" sm="12"></Col>
+                        <Col xl="10" lg="10" md="10" sm="12">
+                            <WrapLogo>
+                                {this.state.images.map((data, key) => (
+                                    <SponsorImage key={key} src={data.src} width={data.width} height={data.height} />
+                                ))}
+                            </WrapLogo>
+                        </Col>
+                        <Col xl="1" lg="1" md="1" sm="12"></Col>
+                    </Row>
+
+                
             </SponsorBg>
         )
     }
