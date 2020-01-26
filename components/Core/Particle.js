ff79ea918bf74745ad08bf7b00f26a1ae026a06c
Akira <31248150+akira2542@users.noreply.github.com>
2020-01-26 21:18:24 +0700

    [UPDATE] Disable mobile particle animation

--- components/Core/Particle.js
+++ components/Core/Particle.js
@@ -15,22 +15,39 @@ const ParticleStyle = {
 
 export default class StarParticle extends Component {
     state={
-        WindowHeight: '2000px'
+        WindowHeight: '2000px',
+        move: true
     }
+    
     componentDidMount() {
         window.addEventListener('resize', this.updateWindowDimensions)
         this.updateWindowDimensions()
+        this.moveCheck()
     }
     updateWindowDimensions = () => {
-        let Height = window.document.body.offsetHeight+"px"
+        let Height = window.document.body.offsetHeight+"px"      
         this.setState({
             WindowHeight: Height
         })
+        this.moveCheck()
+    }
+
+    moveCheck = () => {
+        if (window.innerWidth < 768){
+            this.setState({
+                move: false
+            })   
+        }else{
+            this.setState({
+                move: true
+            })  
+        }
     }
     render() {
+        if (this.state.move){
         return (
 
-            <ParticleContainer>
+            <ParticleContainer key="move">
             <Particles height={this.state.WindowHeight} style={ParticleStyle}
         params={{
             "particles": {
@@ -46,6 +63,7 @@ export default class StarParticle extends Component {
                     "opacity": 0.02
                 },
                 "move": {
+                    "enable": true,
                     "direction": "right",
                     "speed": 0.05
                 },
@@ -80,6 +98,61 @@ export default class StarParticle extends Component {
             {this.props.children}
              </ParticleContainer>
         )
+    }else {
+        return (
+
+            <ParticleContainer key="don't move">
+            <Particles height={this.state.WindowHeight} style={ParticleStyle}
+        params={{
+            "particles": {
+                "number": {
+                    "value": 60,
+                    "density": {
+                        "enable": true,
+                        "value_area": 1500
+                    }
+                },
+                "line_linked": {
+                    "enable": false,
+                    "opacity": 0.02
+                },
+                "move": {
+                    "enable": false,
+                    "direction": "right",
+                    "speed": 0.05
+                },
+                "size": {
+                    "value": 2,
+                    "random": true
+                },
+                "opacity": {
+                    'value': 1,
+                    "anim": {
+                        "enable": true,
+                        "speed": 1,
+                        "opacity_min": 0
+                    }
+                }
+            },
+            "interactivity": {
+                "events": {
+                    "onclick": {
+                        "enable": false,
+                        "mode": "push"
+                    }
+                },
+                "modes": {
+                    "push": {
+                        "particles_nb": 1
+                    }
+                }
+            },
+            "retina_detect": true
+        }} />  
+            {this.props.children}
+             </ParticleContainer>
+        )
+    }
     }
 }
 
