6757554ee4e1493220047c07db24222259c14483
Meawmfia <boon20002543@hotmail.com>
2020-01-26 13:11:49 +0700

    [UPDATE] fix document.js

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default class MyDocument extends Document {
     return (
       <html>
         <title>WIP Camp #12 : Ways to IT Professionals Camp</title>
-        <head>
+        <Head>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
           <meta name="og:title" content="WIP Camp 12" />
@@ -25,7 +25,7 @@ new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-NFZH4C8');` }} /> 
-        </head>
+        </Head>
         <body>
           <Main />
           <NextScript />
