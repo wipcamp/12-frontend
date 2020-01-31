c3d8f3d88e30b70c9df11f3a0c617b8ca839bc10
Pathinya <gift_01210@hotmail.com>
2020-01-31 22:12:10 +0700

    [UPDATE] add parallax item in Track page

--- components/Track/index.js
+++ components/Track/index.js
@@ -5,10 +5,10 @@ import Trackpage from './Track'
 import Flippycardnaja from '../Core/FlippyCard'
 import { Container, Row, Col } from 'reactstrap'
 import CarouselTrack from './Carousel'
+import { Parallax } from 'react-scroll-parallax'
 
 const TrackBg = styled.div`
-    /* background: rgb(9,10,15);
-    background: linear-gradient(90deg, rgba(9,10,15,1) 0%, rgba(27,39,53,1) 50%, rgba(9,10,15,1) 100%); */
+    position: relative;
     padding-bottom: 4rem;
     @media (min-width: 1024px) {
         padding-bottom: 10vh;
@@ -36,6 +36,18 @@ const ButtomContainer = styled.div`
  @media (min-width: 1024px) {
     margin-top:75px
  }
+`
+
+const Star = styled.img`
+    position: absolute;
+    bottom: -8vh;
+    width: 20vw;
+    right: -11vw;
+    display: none;
+
+    @media (min-width: 1024px) {
+        display: block;
+    }
 `
 
 const showCardGrid = () => {
@@ -92,71 +104,74 @@ export default class Track extends Component {
     render() {
         return (
             <TrackBg>
-                <Container>
-                    <Row>
-                        <Col>
-                            <Header color="white" headerText="TRACK" />
-                            <SubHeader color="white" subHeaderText="เส้นทางในค่าย" />
-                        </Col>
-                    </Row>
-
-                    <Row>
-                        <Col xl="1" lg="1" md="1" sm="1" xs="1"></Col>
-                        <Col xl="10" lg="10" md="10" sm="10" xs="10">
-                        <Trackcontainer>
-                        <Content
-                         textIndent="0px"
-                         textIndentIpad="68px"
-                         textIndentDesktop="68px"
-                         textAlign="left"
-                         color="white"
-                         content="แทร็ก (Track) หมายถึง สายอาชีพไอทีที่เปิดโอกาสให้น้อง ๆ ได้เข้ามาสัมผัสและเรียนรู้ตามความสนใจ น้อง ๆ จะได้เลือก Track ตามที่ตนเองสนใจ     
-                         2 Track จากทั้งหมด 4 Track โดย เลือก Track ที่น้องสนใจ ลำดับที่ 1 สำหรับการเลือกเข้าร่วม Track ลำดับที่ 2 จะได้เลือกในวันค่าย" />
-                        </Trackcontainer>
-                        </Col>
-                        <Col xl="1" lg="1" md="1" sm="1" xs="1"></Col>
-                    </Row>
-
-                    <Row id="cards-grid" >
-                        <Col xl='3' lg='3' xs='6'>
-                            <div className=" d-flex justify-content-lg-center justify-content-md-end">
-                                <Flippycardnaja srcFront="/img/FrontCard.png" srcBack="/img/BackCard.png" />
-                            </div>
-                        </Col>
-                        <Col xl='3' lg='3' xs='6'>
-                            <div className=" d-flex justify-content-lg-center justify-content-md-start">
-                                <Flippycardnaja srcFront="/img/FrontCard.png" srcBack="/img/BackCard.png" />
-                            </div>
-                        </Col>
-                        <Col xl='3' lg='3' xs='6'>
-                            <div className=" d-flex justify-content-lg-center justify-content-md-end">
-                                <Flippycardnaja srcFront="/img/FrontCard.png" srcBack="/img/BackCard.png" />
-                            </div>
-                        </Col>
-                        <Col xl='3' lg='3' xs='6'>
-                            <div className=" d-flex justify-content-lg-center justify-content-md-start">
-                                <Flippycardnaja srcFront="/img/FrontCard.png" srcBack="/img/BackCard.png" />
-                            </div>
-                        </Col>
-                    </Row>
-                </Container>
-                <div id="cards-scroll" style={{display: 'none'}}>
-                    <CarouselTrack />
-                </div>
-                <Row>
-                        <Col xl="1" lg="1" md="1" sm="1" xs="1"></Col>
-                        <Col xl="10" lg="10" md="10" sm="10" xs="10">
-                        <ButtomContainer>
-                        <Content
-                        textIndent="0px"
-                        textAlign="left"
-                        textAlignResponsive="center"
-                        color="white"
-                        content="สำหรับการเลือกเข้าร่วม Track ลำดับที่ 2 จะได้เลือกภายในวันค่าย WIP Camp #12" />   
-                        </ButtomContainer>
-                        </Col>
-                        <Col xl="1" lg="1" md="1" sm="1" xs="1"></Col>
-                    </Row>
+                <Parallax y={[0,10]}>
+                    <Parallax y={[0,-10]}>
+                        <Container>
+                            <Row>
+                                <Col>
+                                    <Header color="white" headerText="TRACK" />
+                                    <SubHeader color="white" subHeaderText="เส้นทางในค่าย" />
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col xl="1" lg="1" md="1" sm="1" xs="1"></Col>
+                                <Col xl="10" lg="10" md="10" sm="10" xs="10">
+                                <Trackcontainer>
+                                <Content
+                                 textIndent="0px"
+                                 textIndentIpad="68px"
+                                 textIndentDesktop="68px"
+                                 textAlign="left"
+                                 color="white"
+                                 content="แทร็ก (Track) หมายถึง สายอาชีพไอทีที่เปิดโอกาสให้น้อง ๆ ได้เข้ามาสัมผัสและเรียนรู้ตามความสนใจ น้อง ๆ จะได้เลือก Track ตามที่ตนเองสนใจ     
+                                 2 Track จากทั้งหมด 4 Track โดย เลือก Track ที่น้องสนใจ ลำดับที่ 1 สำหรับการเลือกเข้าร่วม Track ลำดับที่ 2 จะได้เลือกในวันค่าย" />
+                                </Trackcontainer>
+                                </Col>
+                                <Col xl="1" lg="1" md="1" sm="1" xs="1"></Col>
+                            </Row>
+                            <Row id="cards-grid" >
+                                <Col xl='3' lg='3' xs='6'>
+                                    <div className=" d-flex justify-content-lg-center justify-content-md-end">
+                                        <Flippycardnaja srcFront="/img/FrontCard.png" srcBack="/img/BackCard.png" />
+                                    </div>
+                                </Col>
+                                <Col xl='3' lg='3' xs='6'>
+                                    <div className=" d-flex justify-content-lg-center justify-content-md-start">
+                                        <Flippycardnaja srcFront="/img/FrontCard.png" srcBack="/img/BackCard.png" />
+                                    </div>
+                                </Col>
+                                <Col xl='3' lg='3' xs='6'>
+                                    <div className=" d-flex justify-content-lg-center justify-content-md-end">
+                                        <Flippycardnaja srcFront="/img/FrontCard.png" srcBack="/img/BackCard.png" />
+                                    </div>
+                                </Col>
+                                <Col xl='3' lg='3' xs='6'>
+                                    <div className=" d-flex justify-content-lg-center justify-content-md-start">
+                                        <Flippycardnaja srcFront="/img/FrontCard.png" srcBack="/img/BackCard.png" />
+                                    </div>
+                                </Col>
+                            </Row>
+                        </Container>
+                        <div id="cards-scroll" style={{display: 'none'}}>
+                            <CarouselTrack />
+                        </div>
+                        <Row>
+                            <Col xl="1" lg="1" md="1" sm="1" xs="1"></Col>
+                            <Col xl="10" lg="10" md="10" sm="10" xs="10">
+                                <ButtomContainer>
+                                <Content
+                                textIndent="0px"
+                                textAlign="left"
+                                textAlignResponsive="center"
+                                color="white"
+                                content="สำหรับการเลือกเข้าร่วม Track ลำดับที่ 2 จะได้เลือกภายในวันค่าย WIP Camp #12" />   
+                                </ButtomContainer>
+                            </Col>
+                            <Col xl="1" lg="1" md="1" sm="1" xs="1"></Col>
+                        </Row>
+                    </Parallax>
+                    <Star src="/img/Track/star.png" />
+                </Parallax>
             </TrackBg>
         )
     }
