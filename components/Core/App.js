453de4a5d7d9833981fde84fcb1c80241c76ca7c
Pathinya <gift_01210@hotmail.com>
2020-02-05 23:26:20 +0700

    [UPDATE] use conditional rendering with Countdown

--- components/Core/App.js
+++ components/Core/App.js
@@ -1,25 +1,26 @@
 import React, { Component, Fragment } from 'react'
 import Wrapper from './index'
-import Countdown,{zeroPad} from 'react-countdown';
+import Countdown, { zeroPad } from 'react-countdown';
 import ComingSoon from './ComingSoon';
 
-const renderer = ({days ,hours, minutes, seconds, completed }) => {
-    if (completed) {
-      return <Wrapper />;
-    } else {
-      return (
-            <ComingSoon day={zeroPad(days)} hr={zeroPad(hours)} min={zeroPad(minutes)} sec={zeroPad(seconds)} >
-            </ComingSoon>
-      );
-    }
-  };
+const renderer = ({ days, hours, minutes, seconds, completed }) => {
+  return (
+    <Fragment>
+      {
+        completed ?
+          <Wrapper /> 
+          : <ComingSoon day={zeroPad(days)} hr={zeroPad(hours)} min={zeroPad(minutes)} sec={zeroPad(seconds)} />
+      }
+    </Fragment>
+  )
+};
 
 export default class App extends Component {
-    render() {
-        return (
-            <Fragment>
-                <Countdown date='2020-02-12T17:00:00' renderer={renderer} />
-            </Fragment>
-        )
-    }
+  render() {
+    return (
+      <Fragment>
+        <Countdown date='2020-02-12T17:00:00' renderer={renderer} />
+      </Fragment>
+    )
+  }
 }
