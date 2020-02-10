f1a1eea7fed1e2039114cadf59e153db60bdc1fe
Pathinya <gift_01210@hotmail.com>
2020-02-10 23:04:42 +0700

    [UPDATE] conditional rendering in What page

--- components/What/index.js
+++ components/What/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Row, Col} from 'reactstrap'
 import styled from 'styled-components'
@@ -73,11 +73,6 @@ const Cloud1 = styled.img`
     width: 15vw;
     top: 65vh;
     left: -5vw;
-    display: none;
-
-    @media (min-width: 1024px) {
-        display: block;
-    }
 `
 
 const Cloud2 = styled(Cloud1)`
@@ -91,20 +86,33 @@ const Cloud3 = styled(Cloud1)`
     left: 0vw;
     width: 25vw;
 `
+
+const GroupOfCloud = () => {
+    return(
+        <Fragment>
+            <Cloud1 src="/img/What/cloud1.png" data-aos="fade-down-right" />
+            <Cloud2 src="/img/What/cloud2.png" data-aos="fade-down-right" data-aos-duration="1500"/>
+            <Cloud3 src="/img/What/cloud3.png" data-aos="fade-down-right" data-aos-duration="2000"/>
+        </Fragment>
+    )
+}
 export default class What extends Component {
     state = {
+        width: window.innerWidth,
         contentColumn1: 5,
         contentColumn2: 6
     }
     componentDidMount = () => {
         window.addEventListener('resize', this.updateWindowDimensions)
-        this.updateWindowDimensions()
         AOS.init({
             duration: 3000
         })
     }
 
     updateWindowDimensions = () => {
+        this.setState({
+            width: window.innerWidth
+          });
         if (window.innerWidth < 1080) {
             this.setState({
                 contentColumn1: 10,
@@ -117,60 +125,63 @@ export default class What extends Component {
             })
         }
     }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.updateDimensions);
+    }
     render() {
         return (
             <WhatBg>
-                
-                        <Cloud1 src="/img/What/cloud1.png" data-aos="fade-down-right" />
-                        <Cloud2 src="/img/What/cloud2.png" data-aos="fade-down-right" data-aos-duration="1500"/>
-                        <Cloud3 src="/img/What/cloud3.png" data-aos="fade-down-right" data-aos-duration="2000"/>
-                    
-                            <br />
-                            <div className="text-center">
-                                <Header color="white" headerText="WHAT" />
-                            </div>
-                            <SubHeader color="white" subHeaderText="ค่ายนี้คืออะไร ?" />  
-                            <WrapGrid>
-                                <Row>
-                                <Col xl="1" lg="1" md="12" sm="12" xs="12"></Col>
-                                <Col xl="5" lg={this.state.contentColumn1} md="12" sm="12" xs="12">
-                                    <WrapContent>
-                                        <Content
-                                            textIndent="24px"
-                                            textIndentIpad="50px"
-                                            textIndentDesktop="50px"                                
-                                            textAlign="left"
-                                            color="white"
-                                            content="กลับมาอีกครั้งพร้อมกับภารกิจอันยิ่งใหญ่ ซึ่งเปิดโอกาสให้น้อง ๆ 
-                                            ที่มีความสนใจทางด้านไอที หรือกำลังตามหาเส้นทางของตัวเอง 
-                                            มาร่วมกันเดินทางไปพร้อมกัน โดยน้อง ๆ เปรียบเสมือนนักดูดาว 
-                                            ผู้ถูกเลือกเพื่อออกตามหาแสงสว่างที่หายไปจากดาวราศีต่าง ๆ ที่มีเวลาเพียงแค่ 
-                                            5 วัน 4 คืนเท่านั้น "
+                {
+                    (this.state.width < 1024) ?
+                        <Fragment></Fragment>
+                        : <GroupOfCloud />
+                }
+                <br />
+                <div className="text-center">
+                    <Header color="white" headerText="WHAT" />
+                </div>
+                <SubHeader color="white" subHeaderText="ค่ายนี้คืออะไร ?" />  
+                <WrapGrid>
+                    <Row>
+                        <Col xl="1" lg="1" md="12" sm="12" xs="12"></Col>
+                        <Col xl="5" lg={this.state.contentColumn1} md="12" sm="12" xs="12">
+                            <WrapContent>
+                                <Content
+                                    textIndent="24px"
+                                    textIndentIpad="50px"
+                                    textIndentDesktop="50px"                                
+                                    textAlign="left"
+                                    color="white"
+                                    content="กลับมาอีกครั้งพร้อมกับภารกิจอันยิ่งใหญ่ ซึ่งเปิดโอกาสให้น้อง ๆ 
+                                        ที่มีความสนใจทางด้านไอที หรือกำลังตามหาเส้นทางของตัวเอง 
+                                        มาร่วมกันเดินทางไปพร้อมกัน โดยน้อง ๆ เปรียบเสมือนนักดูดาว 
+                                        ผู้ถูกเลือกเพื่อออกตามหาแสงสว่างที่หายไปจากดาวราศีต่าง ๆ ที่มีเวลาเพียงแค่ 
+                                        5 วัน 4 คืนเท่านั้น "
+                                />
+                                <WrapContent2>
+                                    <Content
+                                        textIndent="24px"
+                                        textIndentIpad="50px"
+                                        textIndentDesktop="50px"                                
+                                        textAlign="left"
+                                        color="white"
+                                        content="ในการเดินทางครั้งนี้เหล่านักดูดาว ทั้งหลาย จะมีผู้พิทักษ์ดวงดาวเป็นผู้ชี้นำทาง 
+                                            หนึ่งในภารกิจสำคัญ คือ การตัดสินใจเลือกเส้นทางเดินของตัวเองจากทั้งหมด 4 เส้นทาง ได้แก่ 
+                                            Programming, Web Developer, UX/UI และ Network นอกจากนั้น ยังมีกิจกรรมต่าง ๆ รวมไปถึง 
+                                            IT Management ซึ่งเป็น Workshop พิเศษสำหรับเหล่านักดูดาวทั้งหลาย สุดท้ายแล้ว เรื่องราวครั้งนี้ 
+                                            จะเป็นอย่างไร พวกเขาจะทำภารกิจสำเร็จหรือไม่ ติดตามได้ใน ค่ายเส้นทางสู่ฝันนักไอที ครั้งที่ 12 (WIP Camp #12)"
                                         />
-                                        <WrapContent2>
-                                            <Content
-                                                textIndent="24px"
-                                                textIndentIpad="50px"
-                                                textIndentDesktop="50px"                                
-                                                textAlign="left"
-                                                color="white"
-                                                content="ในการเดินทางครั้งนี้เหล่านักดูดาว ทั้งหลาย จะมีผู้พิทักษ์ดวงดาวเป็นผู้ชี้นำทาง 
-                                                หนึ่งในภารกิจสำคัญ คือ การตัดสินใจเลือกเส้นทางเดินของตัวเองจากทั้งหมด 4 เส้นทาง ได้แก่ 
-                                                Programming, Web Developer, UX/UI และ Network นอกจากนั้น ยังมีกิจกรรมต่าง ๆ รวมไปถึง 
-                                                IT Management ซึ่งเป็น Workshop พิเศษสำหรับเหล่านักดูดาวทั้งหลาย สุดท้ายแล้ว เรื่องราวครั้งนี้ 
-                                                จะเป็นอย่างไร พวกเขาจะทำภารกิจสำเร็จหรือไม่ ติดตามได้ใน ค่ายเส้นทางสู่ฝันนักไอที ครั้งที่ 12 (WIP Camp #12)"
-                                            />
-                                            </WrapContent2>
-                                        </WrapContent>
-                                    </Col>
-                                <Col xl="6" lg={this.state.contentColumn2} md="12" sm="12" xs="12" className="my-auto">
-                                    <WrapSideScroll>
-                                        <WhatSideScroll />
-                                    </WrapSideScroll>
-                                </Col>
-                            </Row>
-                        </WrapGrid>
-                        
+                                </WrapContent2>
+                            </WrapContent>
+                        </Col>
+                        <Col xl="6" lg={this.state.contentColumn2} md="12" sm="12" xs="12" className="my-auto">
+                            <WrapSideScroll>
+                                <WhatSideScroll />
+                            </WrapSideScroll>
+                        </Col>
+                    </Row>
+                </WrapGrid>                
             </WhatBg >
         )
     }
