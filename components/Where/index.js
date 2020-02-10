06e6370edd5ae4b2ae5550af39194423b9e911dc
Pathinya <gift_01210@hotmail.com>
2020-02-11 00:04:11 +0700

    [UPDATE] conditional rendering in Where page

--- components/Where/index.js
+++ components/Where/index.js
@@ -1,15 +1,12 @@
-import React, { Component } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React, { Component, Fragment } from 'react'
 import {
     Container,
     Col,
     Row,
-    Button
 } from 'reactstrap'
 import styled from 'styled-components'
 import { Header, SubHeader, Content } from '../Core/Text'
 import Iframe from 'react-iframe'
-import { ParallaxProvider, Parallax } from 'react-scroll-parallax'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
@@ -64,11 +61,6 @@ const Star1 = styled.img`
     left: 2vw;
     width: 15vw;
     filter: brightness(50%);
-    display: none;
-
-    @media (min-width: 1024px) {
-        display: block;
-    }
 `
 
 const Star2 = styled(Star1)`
@@ -76,9 +68,17 @@ const Star2 = styled(Star1)`
     left: 80vw;
 
 `
-
+const GroupOfStar = () => {
+    return (
+        <Fragment>
+            <Star1 src="/img/Where/star1.svg" data-aos="fade-down-right"/>
+            <Star2 src="/img/Where/star2.svg" data-aos="fade-down-left"/>
+        </Fragment>
+    )
+}
 export default class Where extends Component {
     state = {
+        width: window.innerWidth,
         sideColumn: 3,
         contentColumn: 6
     }
@@ -92,6 +92,9 @@ export default class Where extends Component {
     }
 
     updateWindowDimensions = () => {
+        this.setState({
+            width: window.innerWidth
+          });
         if (window.innerWidth < 1080) {
             this.setState({
                 sideColumn: 2,
@@ -104,39 +107,42 @@ export default class Where extends Component {
             })
         }
     }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.updateDimensions);
+    }
+
     render() {
         return (
 
             <WhereBg>
                 <Container fluid={true}>
-                    
-                        <Star1 src="/img/Where/star1.svg" data-aos="fade-down-right"/>
-                        
-                            <Star2 src="/img/Where/star2.svg" data-aos="fade-down-left"/>
-                        
-                                <Row>
-                                    <Col>
-                                        <Header color="#ffffff" headerText="WHERE" />
-                                        <SubHeader color="#ffffff" subHeaderText="ค่ายจัดที่ไหน ?" />
-                                    </Col>
-                                </Row>
-                                <Row>
-                                    <Col lg={this.state.sideColumn} md="1"></Col>
-                                    <Col lg={this.state.contentColumn} md="10">
-                                        <div className="d-flex justify-content-center">
-                                            <WIPMap url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3877.085040985499!2d100.49142161378397!3d13.652590703265513!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e2a251a57396f5%3A0x5e0d31f39f400b1e!2sWIP%20Camp!5e0!3m2!1sen!2sth!4v1579260821493!5m2!1sen!2sth" />
-                                        </div>
-                                        <SIT />
-                                    </Col>
-                                    <Col lg={this.state.sideColumn} md="1"></Col>
-                                </Row>
-                                <Row>
-                                    <Col className="mt-5">
-                                        <Content textAlign="center" color="white" content="คณะเทคโนโลยีสารสนเทศ มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าธนบุรี">
-                                        </Content>
-                                    </Col>
-                                </Row>
-                            
+                    {
+                        (this.state.width < 1024) ?
+                            <Fragment></Fragment>
+                            : <GroupOfStar />
+                    }
+                    <Row>
+                        <Col>
+                            <Header color="#ffffff" headerText="WHERE" />
+                            <SubHeader color="#ffffff" subHeaderText="ค่ายจัดที่ไหน ?" />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col lg={this.state.sideColumn} md="1"></Col>
+                        <Col lg={this.state.contentColumn} md="10">
+                            <div className="d-flex justify-content-center">
+                                <WIPMap url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3877.085040985499!2d100.49142161378397!3d13.652590703265513!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e2a251a57396f5%3A0x5e0d31f39f400b1e!2sWIP%20Camp!5e0!3m2!1sen!2sth!4v1579260821493!5m2!1sen!2sth" />
+                            </div>
+                            <SIT />
+                        </Col>
+                        <Col lg={this.state.sideColumn} md="1"></Col>
+                    </Row>
+                    <Row>
+                        <Col className="mt-5">
+                            <Content textAlign="center" color="white" content="คณะเทคโนโลยีสารสนเทศ มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าธนบุรี" />>  
+                        </Col>
+                    </Row>        
                 </Container>
             </WhereBg>
         )
