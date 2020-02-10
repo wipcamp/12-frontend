1cc94c2e0071de19b3228ca47a9e89580ea212b3
Pathinya <gift_01210@hotmail.com>
2020-02-11 00:27:08 +0700

    [UPDATE] conditional rendering in Sponsor page

--- components/Sponsor/index.js
+++ components/Sponsor/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { Header, SubHeader } from '../Core/Text'
 import SponsorImage from './SponsorImage'
 import styled from 'styled-components'
@@ -50,24 +50,13 @@ const Star1 = styled.img`
     top: 2vh;
     left: -8vw;
     width: 15vw;
-    display: none;
-
-    @media (min-width: 1280px) {
-        display: block;
-    }
-
 `
 
 const Star2 = styled(Star1)`
     top: 90vh;
     width: 15vw;
 
-    @media (min-width: 1280px) {
-        display: none;
-    }
-
     @media (min-width: 1366px) {
-        display: block;
         left: 92vw;
     }
 
@@ -109,8 +98,21 @@ const Zodiac3 = styled(Zodiac2)`
         left: 91vw;
     }
 `
+
+const GroupOfStar = () => {
+    return(
+        <Fragment>
+            <Star1 src="/img/Sponsor/star1.png" data-aos="fade-down-right"/>
+            <Zodiac1 src="/img/Core/zodiac1.png" data-aos="fade-down-left"/>
+            <Zodiac2 src="/img/Core/zodiac2.png" data-aos="fade-down-left"/>
+            <Zodiac3 src="/img/Sponsor/zodiac3.png" data-aos="fade-down-left"/>
+            <Star2 src="/img/Sponsor/star2.png" data-aos="fade-down-left"/>
+        </Fragment>
+    )
+}
 export default class Sponsor extends Component {
     state = {
+        width: window.innerWidth,
         images: [
             { src: "/img/Sponsor/alibaba.png" },
             { src: "/img/Sponsor/thaibev.png" },
@@ -130,33 +132,43 @@ export default class Sponsor extends Component {
     };
 
     componentDidMount = () => {
+        window.addEventListener('resize', this.updateDimensions);
         AOS.init({
             duration: 3000
         })
     }
 
+    updateDimensions = () => {
+        this.setState({
+            width: window.innerWidth
+          });
+    };
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.updateDimensions);
+    }
+
     render() {
         return (
             <SponsorBg>
-                
-                    <Star1 src="/img/Sponsor/star1.png" data-aos="fade-down-right"/>
-                    <Zodiac1 src="/img/Core/zodiac1.png" data-aos="fade-down-left"/>
-                    <Zodiac2 src="/img/Core/zodiac2.png" data-aos="fade-down-left"/>
-                    <Zodiac3 src="/img/Sponsor/zodiac3.png" data-aos="fade-down-left"/>
-                    <Star2 src="/img/Sponsor/star2.png" data-aos="fade-down-left"/>
-                        <div className="text-center">
-                            <Header color="white" headerText="SPONSORS" />
-                            <SubHeader color="white" subHeaderText="ผู้สนับสนุน" />
-                        </div>
-                        <Container>
-                            <Frame>
-                            <WrapLogo>
-                                {this.state.images.map((data, key) => (
-                                    <SponsorImage key={key} src={data.src} width={data.width} height={data.height} />
-                                ))}
-                            </WrapLogo>
-                            </Frame>
-                        </Container>
+                {
+                    (this.state.width < 1366) ?
+                        <Fragment></Fragment>
+                        : <GroupOfStar />
+                }
+                <div className="text-center">
+                    <Header color="white" headerText="SPONSORS" />
+                    <SubHeader color="white" subHeaderText="ผู้สนับสนุน" />
+                </div>
+                <Container>
+                    <Frame>
+                        <WrapLogo>
+                            {this.state.images.map((data, key) => (
+                                <SponsorImage key={key} src={data.src} width={data.width} height={data.height} />
+                            ))}
+                        </WrapLogo>
+                    </Frame>
+                </Container>
             </SponsorBg>
         )
     }
