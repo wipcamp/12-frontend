bf03be4e9515b24f6a3b35a1759843b7aea30d9e
akira2542 <akiraakamura@gmail.com>
2020-02-12 17:00:49 +0700

    [UPDATE] particle test

--- components/Core/Particle.js
+++ components/Core/Particle.js
@@ -11,11 +11,16 @@ const ParticleContainer = styled.div`
 const ParticleStyle = {
     position:'absolute'
 }
+const ParticleStyleMobile = {
+    position:'fixed'
+}
+
 
 
 export default class StarParticle extends Component {
     state={
         WindowHeight: '2000px',
+        innerHeight: '1000px',
         move: true
     }
     
@@ -28,9 +33,11 @@ export default class StarParticle extends Component {
         window.removeEventListener('resize', this.updateWindowDimensions)
     }
     updateWindowDimensions = () => {
-        let Height = window.document.body.offsetHeight+"px"      
+        let Height = window.document.body.offsetHeight+"px"     
+        let inHeight = window.outerHeight+"px" 
         this.setState({
-            WindowHeight: Height
+            WindowHeight: Height,
+            innerHeight: inHeight
         })
         this.moveCheck()
     }
@@ -114,11 +121,11 @@ export default class StarParticle extends Component {
         return (
 
             <ParticleContainer>
-            <Particles key="don't move" height={this.getHeight()} style={ParticleStyle}
+            <Particles key="don't move" height={this.state.innerHeight} style={ParticleStyleMobile}
         params={{
             "particles": {
                 "number": {
-                    "value": 50,
+                    "value": 120,
                     "density": {
                         "enable": true,
                         "value_area": 1000
@@ -129,17 +136,18 @@ export default class StarParticle extends Component {
                     "opacity": 0.02
                 },
                 "move": {
-                    "enable": false,
-                    "speed": 0
+                    "enable": true,
+                    "direction": "none",
+                    "speed": 0.125
                 },
                 "size": {
                     "value": 2,
                     "random": true
                 },
                 "opacity": {
-                    'value': 1,
+                    'value': 0.8,
                     "anim": {
-                        "enable": false,
+                        "enable": true,
                         "speed": 1,
                         "opacity_min": 0
                     }
