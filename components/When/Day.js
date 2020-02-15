d8f7b4c6ae2afddb1daf98fac2d2679a426312ea
akira2542 <akiraakamura@gmail.com>
2020-02-15 11:35:36 +0700

    [UPDATE] add real day to day.js

--- components/When/Day.js
+++ components/When/Day.js
@@ -1,8 +1,8 @@
 import dayjs from 'dayjs'
 
 export const dayNow = dayjs()
-export const dayOpenRegis = dayjs("2020-02-12")
-export const dayCloseRegis = dayjs("2020-02-12")
-export const dayAnnouce = dayjs("2020-02-12")
-export const dayConfirm = dayjs("2020-02-12")
-export const dayWip = dayjs("2020-02-12")
+export const dayOpenRegis = dayjs("2020-02-19")
+export const dayCloseRegis = dayjs("2020-03-12")
+export const dayAnnouce = dayjs("2020-03-28")
+export const dayConfirm = dayjs("2020-03-29")
+export const dayWip = dayjs("2020-05-27")
