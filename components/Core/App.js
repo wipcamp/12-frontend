ec7b3530235ee11a8be99e8cf2c213ef9a2ece8a
Pathinya <gift_01210@hotmail.com>
2020-02-21 12:24:53 +0700

    [UPDATE] comment the comingsoon page to check performance

--- components/Core/App.js
+++ components/Core/App.js
@@ -3,25 +3,25 @@ import Wrapper from './index'
 import Countdown, { zeroPad } from 'react-countdown';
 import ComingSoon from './ComingSoon';
 
-const renderer = ({ days, hours, minutes, seconds, completed }) => {
-  return (
-    <Fragment>
-      {
-        completed ?
-          <Wrapper /> 
-          : <ComingSoon day={zeroPad(days)} hr={zeroPad(hours)} min={zeroPad(minutes)} sec={zeroPad(seconds)} />
-      }
-    </Fragment>
-  )
-};
+// const renderer = ({ days, hours, minutes, seconds, completed }) => {
+//   return (
+//     <Fragment>
+//       {
+//         completed ?
+//           <Wrapper /> 
+//           : <ComingSoon day={zeroPad(days)} hr={zeroPad(hours)} min={zeroPad(minutes)} sec={zeroPad(seconds)} />
+//       }
+//     </Fragment>
+//   )
+// };
 
 export default class App extends Component {
   render() {
     return (
       <Fragment>
         {/* {/* <Countdown date='2020-02-19T18:00:00' renderer={renderer} /> */}
-        <Countdown date={Date.now()+5000} renderer={renderer} /> 
-  
+        {/* <Countdown date={Date.now()+5000} renderer={renderer} />  */}
+      <Wrapper />
       </Fragment>
     )
   }
